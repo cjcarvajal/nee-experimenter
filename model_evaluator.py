6d0c65712f9a250ec5eb567ddd89c3259dd78666
Carlos Carvajal <ing.carlosj@gmail.com>
2018-11-28 21:02:25 -0500

    Add print function and fix some errors

--- model_evaluator.py
+++ model_evaluator.py
@@ -4,6 +4,7 @@ import json
 import requests
 import unidecode
 import ner
+import sys
 
 remote_mongo_url = 'mongodb://negra:jamaicanGirl@ec2-34-212-201-251.us-west-2.compute.amazonaws.com/nee_experiment'
 client = pymongo.MongoClient(remote_mongo_url)
@@ -31,7 +32,8 @@ def evaluate():
 		update_metrics(tweet['nee_entities'],entities_by_stanford,stanford_metrics)
 		update_metrics(tweet['nee_entities'],entities_by_vision,vision_metrics)
 
-	print_results(stanford_metrics,vision_metrics)
+	print_results(stanford_metrics)
+	print_results(vision_metrics)
 
 def getText(tweet):
 	if 'retweeted_status' in tweet:
@@ -81,10 +83,55 @@ def getEntityFromList(entity,entity_list):
 			return item
 	return None
 
-def print_results(stanford_metrics,vision_metrics):
-	print stanford_metrics
-	print vision_metrics
+result = {u'CITY': {'fp': 34, 'fn': 75, 'tp': 69}, u'CAUSE_OF_DEATH': {'fp': 14, 'fn': 2, 'tp': 2}, u'NATIONALITY': {'fp': 3, 'fn': 18, 'tp': 4}, u'TITLE': {'fp': 79, 'fn': 146, 'tp': 118}, u'PERCENT': {'fp': 2, 'fn': 2, 'tp': 2}, u'COUNTRY': {'fp': 6, 'fn': 13, 'tp': 69}, u'RELIGION': {'fp': 3, 'fn': 1, 'tp': 1}, u'MISC': {'fp': 70, 'fn': 280, 'tp': 10}, u'NUMBER': {'fp': 57, 'fn': 42, 'tp': 50}, u'MONEY': {'fp': 13, 'fn': 37, 'tp': 0}, u'PERSON': {'fp': 102, 'fn': 283, 'tp': 156}, u'IDEOLOGY': {'fp': 14, 'fn': 24, 'tp': 1}, u'LOCATION': {'fp': 63, 'fn': 24, 'tp': 1}, u'CRIMINAL_CHARGE': {'fp': 14, 'fn': 374, 'tp': 1}, u'DATE': {'fp': 21, 'fn': 20, 'tp': 2}, u'ORGANIZATION': {'fp': 260, 'fn': 258, 'tp': 222}, u'STATE_OR_PROVINCE': {'fp': 1, 'fn': 1, 'tp': 1}}
+
+def print_results(metrics):
+	tp_total = 0
+	fp_total = 0
+	fn_total = 0
+	print 70*'-'
+	cols_format = "{:>18} {:<10} {:<10} {:<10} {:<5} {:<5} {:<5}\n"
+	sys.stdout.write(cols_format.format('Entity','P','R','F1','TP', 'FP','FN'))
+	for key in metrics:
+		tp = metrics[key]['tp']
+		fp = metrics[key]['fp']
+		fn = metrics[key]['fn']
+
+		tp_total += tp
+		fp_total += fp
+		fn_total += fn 
+
+		precision = calc_precision(tp,fp)
+		recall = calc_recall(tp,fn)
+		f1 = calc_F1(precision,recall)
+
+		sys.stdout.write(cols_format.format(key,round(precision,8),round(recall,8),round(f1,8),tp,fp,fn))
+
+	precision = calc_precision(tp_total,fp_total)
+	recall = calc_recall(tp_total,fn_total)
+	f1 = calc_F1(precision,recall)
+
+	sys.stdout.write(cols_format.format('TOTAL',round(precision,8),round(recall,8),round(f1,8),tp_total,fp_total,fn_total))
+	print 70*'-'
+	print '\n'
+
+def calc_precision(tp,fp):
+	if tp == 0 and fp == 0:
+		return 0
+	else:
+		return float(tp)/(float(tp + fp))
+
+def calc_recall(tp,fn):
+	if tp == 0 and fn == 0:
+		return 0
+	else:
+		return float(tp)/(float(tp + fn))
+
+
+def calc_F1(precision,recall):
+	if precision == 0 and recall == 0:
+		return 0
+	else:
+		return 2*(precision*recall)/(precision+recall)
 
 evaluate()
-
-
