cbed026128e8da55db44fec8172296b7332c5374
Carlos Carvajal <ing.carlosj@gmail.com>
2018-12-11 09:13:37 -0500

    Add tokenizing of swords which compose a same entity

--- model_input_generator_ordered.py
+++ model_input_generator_ordered.py
@@ -7,6 +7,7 @@ import subprocess
 import spacy
 import re
 import copy
+import sys
 
 nlp = spacy.load('es')
 
@@ -16,12 +17,16 @@ not_entities_percent_length = 0.1;
 number_in_string = True
 money_entity_generator_limit = 1
 money_entity_generator_max = 99
+criminal_charges_generator_limit = 2
 
 #Constants
 regex_entities = r'.*\t(CITY|CAUSE_OF_DEATH|TITLE|COUNTRY|CRIMINAL_CHARGE|RELIGION|MISC|NUMBER|MONEY|PERSON|LOCATION|ORGANIZATION|DATE|NATIONALITY|PERCENT|IDEOLOGY)'
 number_entity = 'NUMBER'
 money_entity = 'MONEY'
-currency_symbols = ['$']
+criminal_charge_entity = 'CRIMINAL_CHARGE'
+person_entity = 'PERSON'
+
+currency_symbols = ['$', '$ ']
 currency_suffix_prefix = ['USD ','COP ','EUR']
 currency_suffix = [' de dolares', ' de pesos', ' de euros']
 money_multiples = [1000000,10000000,100000000,1000000000,10000000000,100000000000,1000000000000]
@@ -42,14 +47,16 @@ training_file_items = []
 test_file_items = []
 
 def process_tweet(text,entities):
+
 	token_list = [text]
 	for entity in entities:
 		dirty_entity = entity['text']
+
 		clean_entity = dirty_entity.strip().lstrip(punctuation).rstrip(punctuation)
 	
 		for index,token in enumerate(token_list):
 			if dirty_entity in token:
-				splitted_text = token.split(clean_entity,1)
+				splitted_text = token.split(clean_entity ,1)
 				if splitted_text[0]:
 					token_list.insert(index,splitted_text[0])
 					index += 1
@@ -67,10 +74,11 @@ def process_tweet(text,entities):
 	return token_list
 
 def tokenize_lines(file_lines):
-	tokenized_file_content = ''
+	tokenized_file_content = []
 	for line in file_lines:
 
 		if not line.strip():
+			tokenized_file_content.append('\n')
 			continue
 		
 		if line:
@@ -78,9 +86,9 @@ def tokenize_lines(file_lines):
 				for token in nlp(line):
 					if not token.text.strip():
 						continue
-					tokenized_file_content += token.text + '\tO\n'
+					tokenized_file_content.append(token.text + '\tO\n')
 			else:
-				tokenized_file_content += line + '\n'
+				tokenized_file_content.append(line + '\n')
 	return tokenized_file_content
 
 total_length = query_result.count()
@@ -114,15 +122,45 @@ def add_money_entities(tweet_list,tweet,money_entity_generator_limit):
 				money_number = randon_number * multiple
 				number_in_string = num2words(randon_number * multiple, lang='es')
 
-				for symbol in currency_symbols:
-					replaced_value = symbol + str(money_number)
-					add_artificial_tweet(tweet_list,tweet,replaced_value,money_value)
-				for suf_pre in currency_suffix_prefix:
-					replaced_value = suf_pre + number_in_string
-					add_artificial_tweet(tweet_list,tweet,replaced_value,money_value)
-				for suf in currency_suffix:
-					replaced_value =number_in_string + suf
-					add_artificial_tweet(tweet_list,tweet,replaced_value,money_value)
+				random_index = random.randint(0,len(currency_symbols) - 1)
+				symbol = currency_symbols[random_index]
+				replaced_value = symbol + str(money_number)
+				add_artificial_tweet(tweet_list,tweet,replaced_value,money_value)
+
+				random_index = random.randint(0,len(currency_suffix_prefix) - 1)
+				suf_pre = currency_suffix_prefix[random_index]
+				replaced_value = suf_pre + number_in_string
+				add_artificial_tweet(tweet_list,tweet,replaced_value,money_value)
+
+				random_index = random.randint(0,len(currency_suffix) - 1)
+				suf = currency_suffix[random_index]
+				replaced_value =number_in_string + suf
+				add_artificial_tweet(tweet_list,tweet,replaced_value,money_value)
+
+def add_criminal_charges(tweet_list,tweet,entity_generator_limit):
+	for nee_entity in tweet['nee_entities']:
+		if nee_entity['type'] == criminal_charge_entity:
+			for counter in range(entity_generator_limit):
+				tweet_list.append(copy.deepcopy(tweet))
+
+def separate_multi_token_entities(tokenized_entity_lines,entity_type):
+	for index,line in enumerate(tokenized_entity_lines):
+		if re.match(regex_entities,line):
+			splited_line = line.split('\t')
+			words = splited_line[0].split()
+			insert_index = index
+			for word in words:
+				tokenized_entity_lines[insert_index] = word + '\t' + splited_line[1]
+				insert_index += 1
+
+def clean_entities(unprocessed_entity_lines):
+	cleaned_entities = []
+	for line in unprocessed_entity_lines:
+		if re.match(r'^\t',line):
+			continue
+		else:
+			cleaned_entities.append(line)
+	return cleaned_entities
 
 retrieved_list = list(query_result)
 
@@ -132,6 +170,7 @@ for item in retrieved_list:
 	entities = item['nee_entities']
 
 	add_money_entities(artificial_tweets,item,money_entity_generator_limit)
+	add_criminal_charges(artificial_tweets,item,criminal_charges_generator_limit)
 
 	'''
 	The order matters so the tweet text must be tokenized and classified following the
@@ -139,14 +178,16 @@ for item in retrieved_list:
 	'''
 	if tweet_counter < training_lenght:
 		training_file_items.extend(process_tweet(full_text,entities))
+		training_file_items.append('\n')
 	else:
-		test_file_items.extend(process_tweet(full_text,entities))	
+		test_file_items.extend(process_tweet(full_text,entities))
+		test_file_items.append('\n')
 
 #Add the artificial tweets to give the algorithm more instances
 tweet_counter = 0
 training_lenght = int(len(artificial_tweets) * training_percent)
 
-#print artificial_tweets
+print len(artificial_tweets)
 
 for item in artificial_tweets:
 	tweet_counter += 1
@@ -155,17 +196,27 @@ for item in artificial_tweets:
 
 	if tweet_counter < training_lenght:
 		training_file_items.extend(process_tweet(full_text,entities))
+		training_file_items.extend('\n')
 	else:
-		test_file_items.extend(process_tweet(full_text,entities))	
+		test_file_items.extend(process_tweet(full_text,entities))
+		test_file_items.extend('\n')
 
 training_file_content = tokenize_lines(training_file_items)
 test_file_content = tokenize_lines(test_file_items)
 
+training_file_content = clean_entities(training_file_content)
+test_file_content = clean_entities(test_file_content)
+
+separate_multi_token_entities(training_file_content,money_entity)
+separate_multi_token_entities(test_file_content,money_entity)
+
 with open ('training.tsv','w') as total_data_file:
-	total_data_file.write(training_file_content.encode('utf8'))
+	for line in training_file_content:
+		total_data_file.write(line.encode('utf8'))
 
 with open ('test.tsv','w') as total_data_file:
-	total_data_file.write(test_file_content.encode('utf8'))
+	for line in test_file_content:
+		total_data_file.write(line.encode('utf8'))
 
 # Remove working files
 # os.system('rm total_data')
